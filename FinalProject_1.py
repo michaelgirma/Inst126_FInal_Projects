7078d424342b69b030f52f0288109db0de7d31b8
Michael Girma <michaelgirma@wireless-10-104-37-78.umd.edu>
2023-12-11 21:27:09 -0500

    added my readme and license files.

---
+++ FinalProject_1.py
@@ -0,0 +1,120 @@
+# We want to bring in user input from a txt file and use it to create a dictionary of words 
+
+# a conditional to stop the loop
+file_condition = False
+
+while file_condition == False:
+    #THIS TRY EXCEPT STRUCTURE BELOW HELPED ME ACCOMPLISH ITEMS 4.2 AND 4.9 OF MY CHECKLIST
+    try: 
+        #provide your file path way
+        file_path = input("Please provide a pathway to a file that ends with .txt and actually exists: ")
+
+        #checks if it is a file that ends with txt
+        if file_path.endswith(".txt"):
+            #checks to see if the txt file can open without a problem so that our except error will run if there are issue 
+            with open(file_path, 'r'):
+                pass
+
+            print("File has succesfully been stored.")
+            file_condition = True
+        else:
+            #if pathway does not end with a txt file raise this error
+            raise ValueError("Please provide a file pathway that ends with a .txt file and actually exists.")
+    except FileNotFoundError:
+        print("This file could not be opened. Please provide a file that exists")
+        file_condition = False
+
+#Now we want to read through that file and store it on a variable 
+with open(file_path, 'r') as user_input:
+
+    #reading through the txt file and putting it in a variable 
+    file_contents = user_input.read()
+    uppercase_file_contents = file_contents.upper()
+
+#should print whatever was on the txt file
+#BY MAKING ALL THE LETTERS THAT WERE READ FROM THE FILE UPPERCASE, I WAS ABLE TO COMPLETE ITEM 3.11
+print("This is the contents of your file: " , uppercase_file_contents)
+
+#now we will split everything in that string based on whitespace
+word_list = uppercase_file_contents.split()
+#BY SPLITTING THIS STRING INTO A LIST OF WORDS I ACCOMPLISHED 7.1
+print("We have created a list of each thing inbetween a space on your file: ", word_list)
+
+#Marks the start of choice 1
+#we will create a list that contains all uppercase alphabets
+#THIS COVERS ITEM 5.7 ON MY CHECKLIST BECASUE IT IS A MANUALLY CREATED LIST
+alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+#stores your response, read carefully
+user_response = input("Type new if you want to make a new list with only words or type remove to remove non-words from the current list: ")
+
+#checks if user type in the right response if not, they have to respond to the prompt again
+if user_response.lower() != 'new' and user_response.lower() != 'remove':
+    print("Please provide the right answer")
+    user_response = input("Type NEW if you want to make a new list with only words or type REMOVE to remove non-words from the current list: ")
+
+#if the user types in new, it will take only words and append them iteratively in a new list
+elif user_response.lower() == 'new':
+    new_word_list = []  
+    counter = 0
+    while counter < len(word_list):
+        found = False
+        #iterates through alphabet
+        for letter in alphabet:
+            if letter in word_list[counter]:#if the current letter we are on is in the current word we are on we append it to the new list
+                #I AM APPENDING AN ITEM TO A LIST ITERATIVELY THROUGH THIS PIECE OF CODE WHICH CHECKES OF ITEM 5.9 AND TECHNICALLY ALSO CHECKS OF 5.8
+                new_word_list.append(word_list[counter])
+                found = True
+                break
+        if not found:
+            print("Not a valid word:", word_list[counter])
+        counter += 1
+    print("Your new list:", new_word_list)
+#if the user types in remove, it will remove anything that is not a word in the original list so all that remains is words
+elif user_response.lower() == 'remove':
+    counter = len(word_list) - 1 #Instead of starting from index 0 we are starting from the last index
+    while counter >= 0: #now the its gonna iterate until we get to the first index in word_list
+        found = False
+        for letter in alphabet:
+            if letter in word_list[counter]:
+                found = True
+                break
+        if not found:
+            print("Not a valid word:", word_list[counter])
+            #I AM REMOVING AN ITEM TO A LIST ITERATIVELY THROUGH THIS PIECE OF CODE WHICH CHECKES OF ITEM 5.11 AND TECHNICALLY 5.10
+            del word_list[counter]  
+        counter -= 1
+# PRINT the modified word_list
+    print("Original Modified word_list:", word_list)
+
+
+#Marks the start of choice 2
+
+while True:
+    choiceTwoPathWay = input("Please provide another pathway that leads to a txt file and exists: ")
+    
+    if choiceTwoPathWay.endswith(".txt"):
+        print("New Path way stored!")
+        break
+    else:
+        print('Please provide a pathway to a .txt file that exists.')
+
+def writeAndApprndToFile(provided_file_path,word_list, new_word_list):
+    if provided_file_path.endswith('.txt'):
+        with open(provided_file_path, 'w') as new_file:
+            new_file.write(word_list)
+            print("List written to file successfully.")
+
+        with open(provided_file_path, 'a') as new_file:
+            new_file.write(new_word_list)
+            print("List appended to file successfully.")
+    else:
+        print("Error: File must be a .txt file.")
+
+string_word_list = ' '.join(word_list)
+print(string_word_list)
+
+string_new_word_list = ' '.join(new_word_list)
+print(string_new_word_list)
+
+writeAndApprndToFile(choiceTwoPathWay, string_word_list, string_new_word_list)
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Inst126_FP_1
-My first Final Project for my INST126 course at UMD. 
+# Word List Processor
+
+#    Description
+1. This Python script allows users to read and process a text file containing words. Users can create a new list containing only words or remove non-words from the original list. Then they can choose to push their results to an already exisiting file or create a new file to append their results to. 
+
+# Prerequisites
+1. You need Python 3 downloaded on your local device
+
+# Getting Started
+1. create a copy of this file or just save the file somewhere on your computer, I would recommend a folder in your desktop
+2. Make sure you have Python installed.
+3. Run the script:
+    1. Find the pathway to the folder your file is in 
+    2. Open you terminal and type cd and paste the pathway right after 
+    3. then type this exactly after if there are no errors: python FinalProject_1.py
+4. After running the script, you will be prompted to provide the path to a text file (ending with .txt). The script will read the file, convert all letters to uppercase, and split the contents into a list of words.
+5. Your first Option:
+    1. Create a New List with Only Words:
+         Type new when prompted. The script will iterate through the words, creating a new list containing only valid words. The resulting list will be displayed.
+
+    2. Remove Non-Words from the Original List
+         Choose option remove when prompted. The script will iteratively remove non-words from the original list. The modified list will be displayed.
+
+6. After that your results will be displayed based on your choice and then you will have the choice after that to write your results to a file or append the results to a file
+
+7. After you make that choice, that would mark the end of this program 
+
+# File Structure
+    1. FinalProject_1.py: The main Python script.
+    2. sample_text_file.txt: An example text file for testing the script. Just create one and type whatever you want in there and let the script do the rest. 
+
+# Note
+    1. Make sure to provide the correct file path and type 'new' or 'remove' when prompted.
+    2. The script is case-insensitive when checking user input.
+
+# Contributing
+    1. Feel free to contribute by opening issues or submitting pull requests. Your feedback and improvements are welcome!
+
+# License
+    1. This project is licensed under the MIT License.
