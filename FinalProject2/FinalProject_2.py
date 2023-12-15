a52a6021be4966d098401fec7a44ea1ba56cf8de
Michael Girma <michaelgirma@wireless-10-104-44-150.umd.edu>
2023-12-15 13:58:51 -0500

    adding a couple more comments

--- FinalProject2/FinalProject_2.py
+++ FinalProject2/FinalProject_2.py
@@ -1,3 +1,6 @@
+#This program below covers all these items below of the checklist
+# 5.15, 5.17, 5.18, 5.19, 6.8, 7.2, 7.3, 7.4, 7.5, 8.1, 8.2, 8.3, 8.4
+
 #BELOW I AM USING ITEM, 6.8: Making appropriate use of another person’s license when incorporating their software. I also put the licenses on the readme file in the root directory
 # Link to pandas license https://pandas.pydata.org/pandas-docs/stable/getting_started/overview.html
 # Link to numpy license https://numpy.org/doc/stable/license.html
@@ -18,6 +21,7 @@ while True:
         print(f"Your csv file does not exist.{e}")
 
     if csv_file_path.endswith('.csv'):
+        #the variable df is now storing all the data from the csv file through the read_csv method from the pandas library
         df = pd.read_csv(csv_file_path)
         print(df)
         break
@@ -29,6 +33,7 @@ keys = my_dict.keys()
 print(keys)
 
 #BELOW I AM USING ITEM, 5.19: Updating values in a dictionary programmatically
+# this is iterating through the rows of a dataframe with the iterrows method, it is appending the values to the corresponding keys in the dictionary
 for index, row in df.iterrows():
     my_dict['Species'].append(row['Species'])
     my_dict['obs'].append(row['obs'])
@@ -37,6 +42,7 @@ for index, row in df.iterrows():
     my_dict['Width'].append(row['Width'])
 
 # BELOW I AM USING ITEM, 5.18: Iterating through the items of a dictionary to access both keys and values
+# .items allows you to access both keys and values, so the variable key is iterating through the keys and the variable value is iterating through the values
 for key, value in my_dict.items():
     print(f"HERE ARE YOUR KEY VALUE PAIRS --> Key: {key}, Value: {value}")
 
@@ -83,8 +89,10 @@ while True:
 import re
 
 while True:
+    #creating a social security pattern below to make sure users are submitting correct social security format
     pattern = r'\d{3}-\d{2}-\d{4}'  
     social_security = input('Please provide a social security number in this format with numbers(abc-ab-abcd). Do not use spaces!: ')
+    #makes sure that the social security number you input mathes up with the pattern variable format
     if re.match(pattern, social_security):
         print("Pattern matched!")
         break
@@ -96,17 +104,21 @@ while True:
     new_social_security = input("If you want to input a new social security type a new one if not just type no: ")
 
     if new_social_security == 'no':
+        #.search is allowing us to search for that social security pattern we have and checks for matches
+        #below it is checking if the variable social-security's value matches up with the pattern we made
         match_result = re.search(r'(\d+)-(\d+)-(\d+)', social_security)
         if match_result:
             groups = match_result.groups()
-            modified_string = f"{int(groups[0])+4}-{int(groups[1])+5}-{int(groups[2])+1}"
+            # to each group of numbers between the - in the social security number u create, the variable below adds to it
+            #for example 345-67-9876, it will take 345 and add 4 to it making it 349
+            modified_string = f"{int(groups[0])+4}-{int(groups[1])+3}-{int(groups[2])+12}"
             print("Heres your original modified social security number: " ,modified_string)
             break
     else:
         new_match_result = re.search(r'(\d+)-(\d+)-(\d+)', new_social_security)
         if new_match_result:
             groups = new_match_result.groups()
-            modified_string = f"{int(groups[0])+4}-{int(groups[1])+5}-{int(groups[2])+1}"
+            modified_string = f"{int(groups[0])+7}-{int(groups[1])+2}-{int(groups[2])+13}"
             print("Heres your new modified social security number: " ,modified_string)
             break
         else:
